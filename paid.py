d33e176011e94cb7aad39dc68c20cc65773808dc
Anii <109001556+ax8n@users.noreply.github.com>
2025-07-02 04:00:41 +0530

    Update paid.py

--- paid.py
+++ paid.py
@@ -3,16 +3,16 @@ import requests
 from datetime import datetime
 from io import StringIO
 
-def vu(ID, token):
+def vu(ID):
     try:
-        url = "https://raw.githubusercontent.com/ax8n/resute/refs/heads/main/access.csv"
+        url = "https://raw.githubusercontent.com/ax8n/aniipy/main/access.csv"
         response = requests.get(url)
         response.raise_for_status()
         csv_data = StringIO(response.text)
         reader = csv.DictReader(csv_data)
 
         for row in reader:
-            if row["user_id"] == str(ID) and row["token"] == token:
+            if row["user_id"] == str(ID):
                 expire = datetime.strptime(row["expire_date"], "%Y-%m-%d %H:%M:%S")
                 if datetime.now() <= expire:
                     return True
@@ -20,8 +20,8 @@ def vu(ID, token):
                     print("✖ Access expired!")
                     print("To renew or buy access, contact: @aniipy")
                     return False
-        print("✖ Invalid token or user ID.")
+        print("✖ Invalid user ID.")
         return False
     except Exception as e:
-        print(f"✖ Error checking access: {e}")
+        print(f"✖ Error checking access")
         return False
